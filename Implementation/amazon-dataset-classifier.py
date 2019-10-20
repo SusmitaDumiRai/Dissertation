5a6138ac4144dd6651b4df33c2e238bb1a4f0487
susmitadumirai <09srai4@gmail.com>
2019-10-20 16:10:26 +0100

    added visualisation methods

--- Implementation/amazon-dataset-classifier.py
+++ Implementation/amazon-dataset-classifier.py
@@ -1,15 +1,58 @@
-# for ease of use, remove missing values.
-import random
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import argparse
-
+# todo import logging
 from glob import glob
 
 
-def visualise_pie(data):
+def random_forest_classifier(data):
+    from sklearn import metrics
+
+    from sklearn.preprocessing import LabelEncoder
+    from sklearn.model_selection import train_test_split
+    from sklearn.ensemble import RandomForestClassifier
+
+    categorical_columns = data.select_dtypes(['object'])
+    # print(list(categorical_columns))
+
+    # data = data.select_dtypes(exclude=['object'])  # for now remove categorical data
+
+    output = ['Label']
+    inputs = [label for label in list(data) if label not in output and label not in categorical_columns]
+
+    le = LabelEncoder()
+    le.fit(data[output])
+
+    X = data[inputs]
+    y = pd.DataFrame(le.transform(data[output]))
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)  # 70% training and 30% test
+    clf = RandomForestClassifier(n_estimators=100)
+    clf.fit(X_train, y_train)
+
+    y_pred = clf.predict(X_test)
+    print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+
+
+def visualise_boxplot(data, save=False):
+    from sklearn.preprocessing import MinMaxScaler
+
+    values = data.values
+    min_max_scaler = MinMaxScaler()
+    values_scaled = min_max_scaler.fit_transform(values)
+    scaled_data = pd.DataFrame(values_scaled,
+                               columns=list(data))
+
+    scaled_data.boxplot(column=list(scaled_data), figsize=(25, 5), rot=90)
+
+    if save:
+        plt.savefig(r'out/boxplot.png')
+
+    plt.show()
+
+
+def visualise_pie(data, save=False):
     protocols = data.groupby(['Label']).size().reset_index(name='count')
 
     protocols['count_norm'] = [float(i) / sum(protocols['count'])
@@ -30,7 +73,11 @@ def visualise_pie(data):
 
     plt.legend(patches, labels, loc='best',
                bbox_to_anchor=(-0.1, 1.), fontsize=10)
-    plt.title('Frequency of protocols')
+    plt.title('Labels')
+
+    if save:
+        plt.savefig(r'out/pie.png')
+
     plt.show()
 
 
@@ -41,7 +88,7 @@ def read_files(files, shuffle=True):
         chunk_list = []
         chunk_size = 10 ** 2  # number of rows to read in one "chunk"
 
-        for j, chunk in enumerate(pd.read_csv(files[i], chunksize=chunk_size, nrows=3000,
+        for j, chunk in enumerate(pd.read_csv(files[i], chunksize=chunk_size, nrows=10,
                                               skiprows=range(1, 5000, 20))):  # todo remove nrows
             print("%s: Chunk process %s" % (i, j))
             chunk = chunk[chunk.Label.str.contains('labels') == False]  # Removes csv rows that have headers repeating
@@ -63,5 +110,12 @@ if __name__ == "__main__":
     # assert len(dataset_files) == 10  # todo remove assert
 
     df = read_files(dataset_files)
-    visualise_pie(df)
+    categorical_columns = df.select_dtypes(['object'])
 
+    numerical_df = df.drop(categorical_columns, axis=1)
+
+    # visualise_pie(df, True)
+    # visualise_boxplot(numerical_df, True)
+    print(df.describe().to_csv(r'out/describe.csv'))
+
+    # random_forest_classifier(df)
