1bbd42b722415ac649ee0084222418c1f3721828
csdog <csdog@110>
2020-02-25 17:58:17 +0000

    fixed small typo

--- Implementation/merge-datasets.py
+++ Implementation/merge-datasets.py
@@ -7,4 +7,4 @@ filenames = [i for i in glob.glob("{0}/*.csv".format(sys.argv[1]))]
 print(filenames)
 print("Number of files: {0}".format(len(filenames)))
 combined_csv = pd.concat([pd.read_csv(f) for f in filenames])
-combined_csv.to_csv("{0}/merged_csv.csv".format(sys.argv[1]), index=False)
+combined_csv.to_csv("{0}/merged-csv.csv".format(sys.argv[1]), index=False)
