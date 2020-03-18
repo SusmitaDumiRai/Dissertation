5b934403ccf4e70c5bc67c99a46664410ac53d12
susi <susi@110.swan.ac.uk>
2020-03-18 12:37:26 +0000

    tried basic conversion

--- Implementation/GAN/gan.py
+++ Implementation/GAN/gan.py
@@ -3,6 +3,7 @@ import argparse
 import logging
 import sys
 import cv2 as cv
+import pandas as pd
 
 from keras.datasets import mnist
 from keras.models import Sequential, Model, Input
@@ -24,10 +25,11 @@ logging.basicConfig(filename=r"out/gan-log.log",  # todo fix this
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-from Implementation.process_data import read_files, normalise_data, drop_columns
+from Implementation.process_data import read_files, drop_columns
 
 z_size = 100  # latent dimension - https://machinelearningmastery.com/how-to-interpolate-and-perform-vector-arithmetic-with-faces-using-a-generative-adversarial-network/
 image_size = 77
+arr = []
 
 def build_model():
   opt_g = Adam(lr=0.0002, beta_1=0.5)
@@ -70,9 +72,10 @@ def train_main(X_train):
 
   print(X_train.shape)
 
-  epochs = 100000
+  epochs = 200
   batch_size = 16
   gan, G, D = build_model()
+
 
   # training
   for e in range(epochs):
@@ -122,6 +125,7 @@ def sample_images(epoch, generator):
 
   # Rescale images 0 - 1
   gen_imgs = 0.5 * gen_imgs + 0.5
+  arr.append(gen_imgs)
   gen_imgs = gen_imgs.reshape(gen_imgs.shape[0], 1, 77)
   gen_imgs = np.tile(gen_imgs, (77, 1))
 
@@ -137,6 +141,15 @@ def sample_images(epoch, generator):
   plt.close()
 
 
+def normalise_data(data):
+  from sklearn import preprocessing
+  values = data.values
+  min_max_scaler = preprocessing.MinMaxScaler()
+  values_scaled = min_max_scaler.fit_transform(values)
+  return pd.DataFrame(values_scaled, columns=list(data)), min_max_scaler
+
+def unnormalise(scaler, data):
+  return scaler.inverse_transform(data)
 
 if __name__ == '__main__':
   handler = logging.StreamHandler(sys.stdout)
@@ -154,7 +167,8 @@ if __name__ == '__main__':
   original_dataset = read_files([args.file_location], clean_data=False)
   original_dataset = drop_columns(original_dataset, ['Label'])
 
-  normalised_data = normalise_data(original_dataset).to_numpy()
+  normalised_data, scaler = normalise_data(original_dataset)
+  normalised_data = normalised_data.to_numpy()
 
   #mean_data = normalised_data.mean(axis=0)
   #mean_data = np.tile(mean_data, (77, 1))
@@ -164,5 +178,11 @@ if __name__ == '__main__':
   #cv.imwrite(r"out/attack.png", 255*mean_data)
 
   train_main(normalised_data)
+  numpy_arr = np.asarray(arr)
+  numpy_arr = numpy_arr.reshape(numpy_arr.shape[0] * numpy_arr.shape[1], numpy_arr.shape[2]) # could transpose instead
+
+  generated_data = unnormalise(scaler, numpy_arr)  # remove first unnecessary column
+
+
 
 
