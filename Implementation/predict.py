5324f059f46b81add8a1559fbf389606277818d6
Susi <908928@swansea.ac.uk>
2020-03-21 17:39:43 +0100

    updated predict for integrated too - needs testing

--- Implementation/predict.py
+++ Implementation/predict.py
@@ -8,17 +8,29 @@ from keras.models import load_model
 from process_data import read_files, drop_columns
 from ensemble import stacked_dataset, load_all_models
 
-from sklearn.metrics import classification_report
 
 # predict using ensemble model
-def ensemble_predict(pretrained_models, ensemble_model, X):
-  # create dataset using ensemble
+def singular_ensemble_predict(X, pretrained_model_paths, ensemble_model_path):
+  pretrained_models = load_all_models(pretrained_model_paths)
+  with open(ensemble_model_path, 'rb') as f:
+    ensemble_model = pickle.load(f)
+
   X_stacked = stacked_dataset(pretrained_models, X)  # produce pretrained model's prediction
-
   y_pred = ensemble_model.predict(X_stacked)  # produce final prediction using ensembles
-  return y_pred
+  u, c = np.unique(y_pred, return_counts=True)
+  print(np.asarray((u, c)))
 
-def nn_predict(X, y, model_paths):
+
+def integrated_ensemble_predict(X, model_path):
+  model = load_model(model_path)
+  X = [X for _ in range(len(model.input))]
+  y_pred = model.predict(X, batch_size=64)
+  y_pred = np.argmax(y_pred, axis=1)
+  u, c = np.unique(y_pred, return_counts=True)
+  print(np.asarray((u, c)))
+
+
+def nn_predict(X, model_paths):
   print(model_paths)
   for model_path in model_paths:
     print(model_path)
@@ -30,17 +42,8 @@ def nn_predict(X, y, model_paths):
     u, c = np.unique(y_pred, return_counts=True)
     print(np.asarray((u, c)))
 
-    # print(y_pred)
-    count_true = np.count_nonzero(y_pred == y[0])
-    # print(count_true)
 
-    # print(y_pred.shape)
-    # print(y.shape)
-    # report = classification_report(y, y_pred)
-    # print(report)
-
-
-def classic_predict(X, y, model_paths):
+def classic_predict(X, model_paths):
   for model_path in model_paths:
     print(model_path)
     with open(model_path, 'rb') as f:
@@ -56,9 +59,10 @@ if __name__ == '__main__':
   parser.add_argument("-d", "--data-location", required=True)
   parser.add_argument("-m", "--model-location")  # for single prediction
   parser.add_argument("-p", "--multiple-model-location")  # multiple pretrained models for ensemble
-
   parser.add_argument("-g", "--ensemble-model-location")
-  parser.add_argument("-e", "--ensemble", action="store_true")
+
+  parser.add_argument("-i", "--integrated-ensemble", action="store_true")
+  parser.add_argument("-s", "--singular-ensemble", action="store_true")
   parser.add_argument("-n", "--neural-network", action="store_true")
   parser.add_argument("-c", "--classic", action="store_true")
   parser.add_argument("-l", "--label-number", type=int, required=True)  # todo easier to make them pass lstm encoding
@@ -75,24 +79,19 @@ if __name__ == '__main__':
     print("Could not drop all columns")
   X = data.to_numpy()
   print(X.shape)
-  print(y.shape)
 
-  if args.ensemble:  # predict using multiple neural network models
-    model_loc = glob(args.multiple_model_location + r"/*.hdf5")
-    assert len(model_loc) >= 2  # atleast one model is required
-    pretrained_models = load_all_models(model_loc)
-    with open(args.ensemble_model_location, 'rb') as f:
-      ensemble_model = pickle.load(f)
-    # evaluate model on test set
-    ensemble_y_pred = ensemble_predict(pretrained_models, ensemble_model, X)
-    u, c = np.unique(ensemble_y_pred, return_counts=True)
-    print(np.asarray((u, c)))
+  if args.singular_ensemble:  # predict using singular ensemble model
+    pretrained_model_loc = glob(args.multiple_model_location + r"/*.hdf5")
+    assert len(pretrained_model_loc) >= 2  # atleast one model is required
+    singular_ensemble_predict(X, pretrained_model_loc, args.ensemble_model_location)
+  elif args.integrated_ensemble:  # predict using integrated model
+    integrated_ensemble_predict(X, args.ensemble_model_location)
   elif args.neural_network:  # predict using singular neural network model
     print("neural network")
-    model_loc = glob(args.model_location + r"/*.hdf5")
-    assert len(model_loc) >= 1
-    nn_predict(X, y, model_loc)
+    nn_model_loc = glob(args.model_location + r"/*.hdf5")
+    assert len(nn_model_loc) >= 1
+    nn_predict(X, nn_model_loc)
   else:  # predict using classic machine learning techniques
-    model_loc = glob(args.model_location + r"/*.sav")
-    assert len(model_loc) >= 1
-    classic_predict(X, y, model_loc)
+    classic_model_loc = glob(args.model_location + r"/*.sav")
+    assert len(classic_model_loc) >= 1
+    classic_predict(X, classic_model_loc)
